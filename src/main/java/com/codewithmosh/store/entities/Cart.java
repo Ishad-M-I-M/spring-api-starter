ee0bb34cc8623e58f6d33bb2d53634361711dd4e
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2025-04-22 12:17:01 +0530

    Capstone Project: Add migrations script

---
+++ src/main/java/com/codewithmosh/store/entities/Cart.java
@@ -0,0 +1,40 @@
+package com.codewithmosh.store.entities;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Generated;
+
+import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "date_created", insertable = false, updatable = false)
+    private LocalDate dateCreated;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE)
+    private Set<CartItem> cartItems = new LinkedHashSet<>();
+
+}
